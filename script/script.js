9a35ce63a79e6cbbfeb22835eaf5e55e773062e5
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-03-06 00:19:52 +0300

    This commit will declare variables for date, month, year, gender and day of the week.

--- script/script.js
+++ script/script.js
@@ -1,2 +1,13 @@
 var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+function validateForm(){
+    var dd = parseInt(document.getElementById('date').value);
+    var mm = parseInt(document.getElementById('month').value);
+    var year = document.getElementById('year').value;
+    var yy = parseInt(year.substr(2, 4));
+    var cc = parseInt(year.substr(0, 2));
+    var gender = document.getElementById('gender').value;
+    var d= parseInt(((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )% 7;
+
+}
